4e0b50fdd73645cdc6bfd0fb93e47f78fd719a57
SU <valej200554@gmail.com>
2023-10-28 07:28:59 +0500

    addd

---
+++ src/pages/Home/Keywords/Keywords.jsx
@@ -0,0 +1,49 @@
+import style from "./Keywords.module.css"
+
+function Keywords() {
+  return (
+    <div className={style.KeywordsBlock}>
+      <div className={style.KeywordsInside}>
+        <div className={style.KeywordsHeaderText}>
+          <p>Для успешного обучения</p>
+          <h1>Ключевые преимущества</h1>
+        </div>
+        <div className={style.KeywordsMain}>
+          <ol>
+            <li>Библиотека готовых уроков</li>
+            <div>
+              <img src="" alt="icon" />
+              <p>ghghghghghghgh</p>
+            </div>
+            <li>Библиотека готовых уроков</li>
+            <div>
+              <img src="" alt="icon" />
+              <p>ghghghghghghgh</p>
+            </div>
+            <li>Библиотека готовых уроков</li>
+            <div>
+              <img src="" alt="icon" />
+              <p>ghghghghghghgh</p>
+            </div>
+            <li>Библиотека готовых уроков</li>
+            <div>
+              <img src="" alt="icon" />
+              <p>ghghghghghghgh</p>
+            </div>
+            <li>Библиотека готовых уроков</li>
+            <div>
+              <img src="" alt="icon" />
+              <p>ghghghghghghgh</p>
+            </div>
+            <li>Библиотека готовых уроков</li>
+            <div>
+              <img src="" alt="icon" />
+              <p>ghghghghghghgh</p>
+            </div>
+          </ol>
+        </div>
+      </div>
+    </div>
+  )
+}
+export default Keywords
