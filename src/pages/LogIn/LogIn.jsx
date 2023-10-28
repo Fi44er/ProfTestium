81764aceb1e0aebcd61aa33cbeb352239029a4d1
Fi44er <vaceslavmalugin81@gmail.com>
2023-10-28 11:27:55 +0300

    add

--- src/pages/LogIn/LogIn.jsx
+++ src/pages/LogIn/LogIn.jsx
@@ -12,9 +12,20 @@ const LogIn = () => {
       <form onSubmit={handleSubmit} className={style.logForm}>
         <h1>Войти</h1>
         <div className={style.inpBlock}>
-          <Input placeInp="Логин, почта или телефон" backInp='rgba(255, 255, 255, 0.5)'/>
-          <Input typeBtn="password" placeInp="Ваш пароль" />
-          <Button backBtn='#fff' colorBtn='#d31313'>Войти</Button>
+          <Input
+            placeInp="Логин, почта или телефон"
+            backInp="rgba(255, 255, 255, 0.5)"
+            colorInp="#000"
+          />
+          <Input
+            typeBtn="password"
+            placeInp="Ваш пароль"
+            backInp="rgba(255, 255, 255, 0.5)"
+            colorInp="#000"
+          />
+          <Button backBtn="#fff" colorBtn="#000">
+            Войти
+          </Button>
         </div>
       </form>
     </div>
