04f6ea91f35687f348101e4b1d93f37c2da70f74
Fi44er <vaceslavmalugin81@gmail.com>
2023-10-28 06:35:17 +0300

    add

--- src/pages/Home/Keywords/Keywords.jsx
+++ src/pages/Home/Keywords/Keywords.jsx
@@ -1,46 +1,46 @@
+import Cart from "../../../components/globalComponents/Cart/Cart"
 import style from "./Keywords.module.css"
+
+const cartObj = [
+  {
+    number: '01',
+    heading: 'Библиотека готовых уроков',
+    info: 'Сотни материалов для увлекательных уроков',
+    icon: '/src/assets/icon/book.svg'
+  },
+  {
+    number: '02',
+    heading: 'Видеочат',
+    info: 'Попробуй наше удобное видео общение',
+    icon: '/src/assets/icon/camera.svg'
+  },
+  {
+    number: '03',
+    heading: 'Виртуальная доска',
+    info: 'Иновационный инструмент для повышения мотивации',
+    icon: '/src/assets/icon/board.svg'
+  }
+]
 
 function Keywords() {
   return (
     <div className={style.KeywordsBlock}>
       <div className={style.KeywordsInside}>
         <div className={style.KeywordsHeaderText}>
-          <p>Для успешного обучения</p>
           <h1>Ключевые преимущества</h1>
         </div>
         <div className={style.KeywordsMain}>
-          <ol>
-            <li>Библиотека готовых уроков</li>
-            <div>
-              <img src="" alt="icon" />
-              <p>ghghghghghghgh</p>
-            </div>
-            <li>Библиотека готовых уроков</li>
-            <div>
-              <img src="" alt="icon" />
-              <p>ghghghghghghgh</p>
-            </div>
-            <li>Библиотека готовых уроков</li>
-            <div>
-              <img src="" alt="icon" />
-              <p>ghghghghghghgh</p>
-            </div>
-            <li>Библиотека готовых уроков</li>
-            <div>
-              <img src="" alt="icon" />
-              <p>ghghghghghghgh</p>
-            </div>
-            <li>Библиотека готовых уроков</li>
-            <div>
-              <img src="" alt="icon" />
-              <p>ghghghghghghgh</p>
-            </div>
-            <li>Библиотека готовых уроков</li>
-            <div>
-              <img src="" alt="icon" />
-              <p>ghghghghghghgh</p>
-            </div>
-          </ol>
+          {cartObj.map(item => {
+            return (
+              <Cart
+              icon={item.icon}
+              number={item.number}
+              heading={item.heading}
+              info={item.info}
+            />
+            )
+          })}
+          
         </div>
       </div>
     </div>
